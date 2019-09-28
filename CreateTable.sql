870435f157951b3350ffd7e49aeb33634b73fb8a
cenker.karaors <cenker.karaors@ozu.edu.tr>
2019-09-28 14:02:06 +0300

    SQL init

---
+++ CreateTable.sql
@@ -0,0 +1,70 @@
+use [cs320DB]
+
+/* Customer zipcode city */
+CREATE TABLE Customer_ZC (
+zipcode INT NOT NULL,
+city VARCHAR(15) NOT NULL,
+PRIMARY KEY(zipcode)
+);
+
+/* Customer address zipcode */
+CREATE TABLE Customer_AZ (
+caddress VARCHAR(30) NOT NULL,
+zipcode INT NOT NULL,
+PRIMARY KEY(caddress),
+FOREIGN KEY (zipcode) REFERENCES Customer_ZC(zipcode)
+);
+
+/* Customer */
+CREATE TABLE Customer (
+username VARCHAR(15) NOT NULL,
+cname VARCHAR(15) NOT NULL,
+last_name VARCHAR(15) NOT NULL,
+phone VARCHAR(12) NOT NULL,
+caddress VARCHAR(30) NOT NULL,
+PRIMARY KEY (username),
+FOREIGN KEY (caddress) REFERENCES Customer_AZ(caddress)
+);
+
+/* Customers Email */
+CREATE TABLE Email (
+email VARCHAR(30) NOT NULL,
+username VARCHAR(15) NOT NULL,
+PRIMARY KEY (email),
+FOREIGN KEY (username) REFERENCES Customer(username)
+);
+
+/* Item */
+CREATE TABLE Item (
+item_id INT NOT NULL,
+item_name VARCHAR(15) NOT NULL,
+item_desc TEXT NOT NULL, /* Item Description */
+stock INT NOT NULL,
+price FLOAT NOT NULL,
+photo BLOB NOT NULL,
+PRIMARY KEY (item_id)
+);
+
+/* Basket */
+CREATE TABLE Basket (
+basket_id INT NOT NULL,
+username VARCHAR(15) NOT NULL,
+PRIMARY KEY (basket_id),
+FOREIGN KEY (username) REFERENCES Customer(username)
+);
+
+/* Basket Contains */
+CREATE TABLE BContains (
+basket_id INT NOT NULL,
+item_id INT NOT NULL,
+amount INT NOT NULL,
+FOREIGN KEY (basket_id) REFERENCES Basket(basket_id),
+FOREIGN KEY (item_id) REFERENCES Item(item_id)
+);
+
+/* History(Sold Baskets) */
+CREATE TABLE History (
+basket_id INT NOT NULL,
+FOREIGN KEY (basket_id) REFERENCES Basket(basket_id)
+);
+
